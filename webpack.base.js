6469941c20a3e881b79b88668af1400a8a0cf6c9
Tristan Teufel <tristanteufel@googlemail.com>
2017-06-30 22:41:35 +0200

    webpack

--- webpack.base.js
+++ webpack.base.js
@@ -24,10 +24,6 @@ module.exports = {
             {
                 test: /\.(eot|svg|ttf|woff|woff2)$/,
                 use: [{loader: 'file-loader?name=font/[name]__[hash:base64:5].[ext]'}]
-            },
-            {
-                test: /\.node$/,
-                use: 'file-loader'
             }
         ]
     },

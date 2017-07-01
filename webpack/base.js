67ca5c0381c99cedb46457d5f83d23888dc273bb
Tristan Teufel <tristanteufel@googlemail.com>
2017-07-01 14:49:06 +0200

    output path

--- webpack/base.js
+++ webpack/base.js
@@ -7,7 +7,7 @@ module.exports = {
         gui: './src/index.tsx',
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, '../dist'),
         filename: '[name].js'
     },
     module: {
